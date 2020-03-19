2f9dc559daa9c67a98a460f8dac23d1b95a46fc7
Ariane Lima <arianef.lima@yahoo.com.br>
2020-03-19 15:23:51 -0300

    Incluir e salvar informações em banco de dados do servidor aulas 2.3, 2.4 e 2.5

--- src/main/java/com/gft/cobranca/CobrancaApplication.java
+++ src/main/java/com/gft/cobranca/CobrancaApplication.java
@@ -2,12 +2,22 @@ package com.gft.cobranca;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.i18n.FixedLocaleResolver;
+
+import java.util.Locale;
+
 
 @SpringBootApplication
 public class CobrancaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CobrancaApplication.class, args);
+	}
+	
+	@Bean
+	public FixedLocaleResolver localeResolver() {
+		return new FixedLocaleResolver(new Locale("pt", "BR"));
 	}
 
 }
