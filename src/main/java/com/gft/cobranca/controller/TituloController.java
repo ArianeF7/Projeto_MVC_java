68bdac056e6799b54ba648f4ed8e95e0743b7d9c
Ariane Lima <arianef.lima@yahoo.com.br>
2020-03-26 11:11:59 -0300

    Ajustes de responsividade e layout, Finalização do curso de MVC-aula 3.12

--- src/main/java/com/gft/cobranca/controller/TituloController.java
+++ src/main/java/com/gft/cobranca/controller/TituloController.java
@@ -18,6 +18,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import com.gft.cobranca.model.StatusTitulo;
 import com.gft.cobranca.model.Titulo;
 import com.gft.cobranca.repository.Titulos;
+import com.gft.cobranca.repository.filter.TituloFilter;
 import com.gft.cobranca.service.CadastroTituloService;
 
 @Controller
@@ -58,8 +59,10 @@ public class TituloController {
 	}
 	
 	@RequestMapping
-	public ModelAndView pesquisar() {
-		List<Titulo> todosTitulos = titulos.findAll();
+	public ModelAndView pesquisar(@ModelAttribute("filtro") TituloFilter filtro) {		
+		
+		List<Titulo> todosTitulos = cadastroTituloService.filtrar(filtro);		
+		
 		ModelAndView mv = new ModelAndView("PesquisaTitulos");
 		mv.addObject("titulos", todosTitulos);
 		return mv;
